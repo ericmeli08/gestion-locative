054624201d7781eb438cfeef002b39745ce34632
melieric08 <fouegangmeli@gmail.com>
2025-09-29 03:29:52 +0100

    change port

--- Start-App.bat
+++ Start-App.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Lancement de l'application de gestion locative...
 cd /d C:\gestion-locative
-start "" http://localhost:8000
+start "" http://localhost:80001
 timeout /t 6 > nul
 docker compose up
