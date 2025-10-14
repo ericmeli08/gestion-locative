4346f67dd12527d267dc428473282fce30a816e4
melieric <fouegangmeli@gmail.com>
2025-10-14 01:12:32 +0100

    Uncomment CheckRecurringCharges middleware in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(web: __DIR__ . '/../routes/web.php', commands: __DIR__ . '/../routes/console.php', health: '/up')
     ->withMiddleware(function (Middleware $middleware): void {
 
-        // $middleware->append(\App\Http\Middleware\CheckRecurringCharges::class);
+        $middleware->append(\App\Http\Middleware\CheckRecurringCharges::class);
 
         $middleware->web(append: [HandleInertiaRequests::class, \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class]);
 
